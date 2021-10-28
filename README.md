49671e315fdd3fa7253d918e71bd460bc1b04caf
theglitchmaster64 <theglitchmaster64@gmail.com>
2021-10-28 23:15:02 +0200

    venv setup

---
+++ README.md
@@ -0,0 +1,1 @@
+files for acml assignments
---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+
+if __name__=='__main__':
+	sys.stdout.write('STDOUT TEST QWERTYUIOPASDFGHJKLZXCVBNM')
+	sys.stderr.write('STDERR TEST QWERTYUIOPASDFGHJKLZXCVBNM')
+	sys.exit(0)
