e8d7a2afed031f55fd81b74404f702bfb15109b7
theglitchmaster64 <theglitchmaster64@gmail.com>
2021-11-07 00:26:23 +0100

    ...

--- assignment2/classes.py
+++ assignment2/classes.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
-
+import string
+import random
+import os
 
 # load n samples from samples folder and rescale to 32x32
 # m splits the data into train and test, ex - m=8 is a 80-20 split
@@ -9,7 +11,7 @@ class LoadSamples:
 	def __init__(self,n,m):
 		self.samples = []
 		for i in range(1,n+1):
-			self.samples.append({'col':cv2.resize(cv2.imread('samples/{}.jpg'.format(i)),(32,32)),'grey':cv2.resize(cv2.imread('samples/{}.jpg'.format(i),cv2.IMREAD_GRAYSCALE),(32,32))})
+			self.samples.append({'col':cv2.resize(cv2.imread('samples/{}.jpg'.format(i)),(32,32))/255.0,'grey':cv2.resize(cv2.imread('samples/{}.jpg'.format(i),cv2.IMREAD_GRAYSCALE),(32,32))/255.0})
 		self.train = self.samples[0:m]
 		self.test = self.samples[m::]
 
@@ -17,3 +19,24 @@ class LoadSamples:
 		for i in range(len(self.samples)):
 			cv2.imwrite('tmp/bw_{}.jpg'.format(i),self.samples[i]['grey'])
 			cv2.imwrite('tmp/{}.jpg'.format(i),self.samples[i]['col'])
+
+class OutputImages:
+	def __init__(self,name=None,elems=None):
+		if elems == None:
+			self.items = []
+		else:
+			self.items = elems
+		if name == None:
+			self.name = ''.join([chr(random.randrange(97,122)) for x in range(8)])
+		else:
+			self.name = name
+
+	def add_image(self,img):
+		self.items.append(img)
+
+	def write_images(self):
+		if os.path.exists(os.path.join(os.getcwd(),self.name)):
+			print('path already exists! aborting...')
+			return None
+		else:
+			os.mkdir(os.path.join(os.getcwd(),self.name))
--- assignment2/main.py
+++ assignment2/main.py
@@ -7,6 +7,48 @@ import cv2
 from classes import *
 from matplotlib import pyplot as plt
 
+
 if __name__=='__main__':
-	print('supm8')
-	x = LoadSamples(10,8)
+
+	print('ok')
+
+	input_shape = (32,32,3)
+	red_dim = 256
+	layer_filters = [64,128,256]
+
+	#encoder stub
+	print('preparing encoder stub...')
+	input_layer = keras.layers.Input(shape=input_shape)
+	encoder_layer = input_layer
+	for nlayer in layer_filters:
+		encoder_layer = keras.layers.Conv2D(filters = nlayer, kernel_size = 3,strides=1,activation='relu',padding='same')(encoder_layer)
+	shape = keras.backend.int_shape(encoder_layer)
+	encoder_layer = keras.layers.Flatten()(encoder_layer)
+	encoder_output = keras.layers.Dense(red_dim)(encoder_layer)
+	encoder = keras.models.Model(input_layer,encoder_output,name='enc')
+
+	#decoder stub
+	print('preparing decoder stub...')
+	decoder_inputs = keras.layers.Input(shape=red_dim)
+	decoder_layer = keras.layers.Dense(shape[1]*shape[2]*shape[3])(decoder_inputs)
+	decoder_layer = keras.layers.Reshape((shape[1],shape[2],shape[3]))(decoder_layer)
+	for nlayer in layer_filters[::-1]:
+		decoder_layer = keras.layers.Conv2DTranspose(filters=nlayer,kernel_size = 3, strides = 1, activation='relu', padding='same')(decoder_layer)
+	decoder_output = keras.layers.Conv2DTranspose(filters=3,kernel_size = 3, strides = 1, activation='sigmoid', padding='same')(decoder_layer)
+	decoder = keras.models.Model(decoder_inputs,decoder_output,name='dec')
+
+	ae = keras.models.Model(input_layer,decoder(encoder(input_layer)),name='autoenc')
+
+	print('loading samples...')
+
+	X = LoadSamples(10,8)
+	x_train = X.train['col']
+	x_test = X.test['col']
+	x_train_grey = X.train['grey']
+	x_test_grey = X.test['grey']
+
+	print('training model...')
+
+	ae.fit(x_train_grey,x_train,validation_data=(x_test_grey,x_test),epochs=10,batch_size=4)
+
+	print('alldone!')
